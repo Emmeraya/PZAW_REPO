3685f228f8c919a143807b07fab899a3051c9865
Weronika Wiśniewska <sto.wisniewskaw@gmail.com>
2025-10-08 06:20:40 +0000

    jest favicon

--- projekt-001/src/path_handlers.js
+++ projekt-001/src/path_handlers.js
@@ -1,7 +1,7 @@
 import { readFileSync } from "node:fs";
 
 const index_html = readFileSync("./sitrese/index.html");
-const favicon_ico =readFileSync("./sitrese/favicon.ico")
+const favicon_ico =readFileSync("./sitrese/favicon.ico");
 
 const pathConfigs = [
   {
@@ -22,6 +22,7 @@ const pathConfigs = [
   },
   {
     path: "/favicon.ico",
+    allowed_methods: ["GET"],
     handler: (req, res) => {
       res.writeHead(200, { "Content-Type": "image/vnd.microsoft.icon" });
       res.end(favicon_ico);
