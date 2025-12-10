1853726aef0a2e58d61ab36b3a04a990fbeb2fec
Weronika Wiśniewska <sto.wisniewskaw@gmail.com>
2025-12-10 08:12:17 +0000

    please just work (help me)

--- projekt-002/galleri/pics.js
+++ projekt-002/galleri/pics.js
@@ -22,6 +22,9 @@ const db_ops = {
     `INSERT INTO pc_categs (id, name)
         VALUES (?, ?) RETURNING category_id, id, name;`
   ),
+  update_category_by_id: db.prepare(
+    "UPDATE pc_categs SET id = $new_category_id, name = $name WHERE id = $category_id RETURNING category_id, id, name;"
+  ),
   insert_kitty: db.prepare(
     `INSERT INTO pc_kitties (category_id, ascii_art) 
         VALUES (?, ?) RETURNING id, ascii_art;`
@@ -38,6 +41,13 @@ const db_ops = {
     "SELECT category_id, id, name FROM pc_categs WHERE id = ?;"
   ),
   get_kitty_by_category_id: db.prepare(
+    "SELECT id, ascii_art FROM pc_kitties WHERE category_id = ?;"
+  ),
+  update_kitty_by_id: db.prepare(
+    "UPDATE pc_kitties SET ascii_art = ? WHERE id = ? RETURNING id, ascii_art;"
+  ),
+  delete_kitty_by_id: db.prepare("DELETE FROM pc_kitties WHERE id = ?;"),
+  get_kitties_by_category_id: db.prepare(
     "SELECT id, ascii_art FROM pc_kitties WHERE category_id = ?;"
   ),
 };
@@ -68,6 +78,11 @@ export function hasCategory(categoryId) {
   return category != null;
 }
 
+export function hasKitty(kittyId) {
+  let category = db_ops.get_kitty_by_id.get(kittyId);
+  return category != null;
+}
+
 export function getCategory(categoryId) {
   let category = db_ops.get_category_by_id.get(categoryId);
   if (category != null) {
@@ -81,8 +96,25 @@ export function addKitty(categoryId, kitty) {
   return db_ops.insert_kitty_by_id.get(categoryId, kitty.ascii_art);
 }
 
+
+export function updateKitty(kitty) {
+  return db_ops.update_kitty_by_id.get(kitty.ascii_art, kitty.id);
+}
+
+export function deleteKittyById(kittyId) {
+  return db_ops.delete_kitty_by_id.run(kittyId);
+}
+
 export function addCategory(categoryId, name) {
   return db_ops.insert_category.get(categoryId, name);
+}
+
+export function updateCategory(categoryId, newCategoryId, name) {
+  return db_ops.update_category_by_id.get({
+    $category_id: categoryId,
+    $new_category_id: newCategoryId,
+    $name: name,
+  });
 }
 
 
@@ -103,11 +135,39 @@ export function validateKittyData(kitty) {
   return errors;
 }
 
+export function validateCategoryName(name) {
+  var errors = [];
+  if (typeof name != "string") {
+    errors.push("Category name should be a string");
+  } else {
+    if (name.length < 3 || name.length > 100) {
+      errors.push("Category name should have 3-100 characters");
+    }
+  }
+
+  return errors;
+}
+
+export function generateCategoryId(name) {
+  const categoryId = name
+    .toLowerCase()
+    .replace(/(\s|[.-])+/g, "-")
+    .replace(/[^a-z0-9.-]/g, "");
+
+  return categoryId;
+}
+
 export default {
   getCategorySummaries,
   hasCategory,
   getCategory,
+  hasKitty,
   addKitty,
+  updateKitty,
+  deleteKittyById,
   addCategory,
+  updateCategory,
   validateKittyData,
+  validateCategoryName,
+  generateCategoryId,
 };
